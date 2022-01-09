f70793019bf699c52c2c67432483c7262916beb2
GAROKART <80225913+GAROKART@users.noreply.github.com>
2022-01-09 01:42:27 +0100

    Add files via upload

---
+++ Windows activator.bat
@@ -0,0 +1,304 @@
+@echo off
+title Windows Activator by Dinkelberg#0001
+echo Du musst die batch als admin starten falls du das bereits gemacht hast kannst du Enter druecken
+pause
+cls
+echo        --WINDOWS VERSION AUSWAEHLEN--
+echo Windows Home -1-
+echo Windows Home N -2-
+echo Home Single Language -3-
+echo Home Country Specific -4-
+echo ---------------------------
+echo Pro -5-
+echo Pro N -6-
+echo Pro Workstations -7-
+echo Pro Workstations N -8-
+echo Pro Education -9-
+echo ---------------------------
+echo Education -10-
+echo Education N -11-
+echo ---------------------------
+echo Enterprise -12-
+echo Enterprise N -13-
+echo Enterprise G -14-
+echo Enterprise G N -15-
+echo Enterprise LTSC 2019 -16-
+echo Enterprise N LTSC 2019 -17-
+echo Enterprise LTSB 2016 -18-
+echo Enterprise N LTSB 2016 -19-
+echo ---------------------------
+echo Windows 10 Core -20-
+echo Windows 10 S (Lean) -21-
+echo Windows 10 Pro build 10240 -22-
+set /p uni= Option in Zahl eintippen:
+if %uni% ==1 goto :1
+if %uni% ==2 goto :2
+if %uni% ==3 goto :3
+if %uni% ==4 goto :4
+if %uni% ==5 goto :5
+if %uni% ==6 goto :6
+if %uni% ==7 goto :7
+if %uni% ==8 goto :8
+if %uni% ==9 goto :9
+if %uni% ==10 goto :10
+if %uni% ==11 goto :11
+if %uni% ==12 goto :12
+if %uni% ==13 goto :13
+if %uni% ==14 goto :14
+if %uni% ==15 goto :15
+if %uni% ==16 goto :16
+if %uni% ==17 goto :17
+if %uni% ==18 goto :18
+if %uni% ==19 goto :19
+if %uni% ==20 goto :20
+if %uni% ==21 goto :21
+if %uni% ==22 goto :22
+
+:1
+cls
+pause
+slmgr /ipk <TX9XD-98N7V-6WMQ6-BX7FG-H8Q99>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:2
+cls
+pause
+slmgr /ipk <3KHY7-WNT83-DGQKR-F7HPR-844BM>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:3
+cls
+pause
+slmgr /ipk <7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:4
+cls
+pause
+slmgr /ipk <PVMJN-6DFY6-9CCP6-7BKTT-D3WVR>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:5
+cls
+pause
+slmgr /ipk <W269N-WFGWX-YVC9B-4J6C9-T83GX>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:6
+cls
+pause
+slmgr /ipk <MH37W-N47XK-V7XM9-C7227-GCQG9>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:7
+cls
+pause
+slmgr /ipk <NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:8
+cls
+pause
+slmgr /ipk <9FNHH-K3HBT-3W4TD-6383H-6XYWF>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:9
+cls
+pause
+slmgr /ipk <6TP4R-GNPTD-KYYHQ-7B7DP-J447Y>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:10
+cls
+pause
+slmgr /ipk <NW6C2-QMPVW-D7KKK-3GKT6-VCFB2>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:11
+cls
+pause
+slmgr /ipk <2WH4N-8QGBV-H22JP-CT43Q-MDWWJ>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:12
+cls
+pause
+slmgr /ipk <NPPR9-FWDCX-D2C8J-H872K-2YT43>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:13
+cls
+pause
+slmgr /ipk <DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:14
+cls
+pause
+slmgr /ipk <YYVX9-NTFWV-6MDM3-9PT4T-4M68B>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:15
+cls
+pause
+slmgr /ipk <44RPN-FTY23-9VTTB-MP9BX-T84FV>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:16
+cls
+pause
+slmgr /ipk <M7XTQ-FN8P6-TTKYV-9D4CC-J462D>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:17
+cls
+pause
+slmgr /ipk <92NFX-8DJQP-P6BBQ-THF9C-7CG2H>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:18
+cls
+pause
+slmgr /ipk <DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:19
+cls
+pause
+slmgr /ipk <QFFDN-GRT3P-VKWWX-X7T3R-8B639>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:20
+cls
+pause
+slmgr /ipk <33QT6-RCNYF-DXB4F-DGP7B-7MHX9>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:21
+cls
+pause
+slmgr /ipk <NBTWJ-3DR69-3C4V8-C26MC-GQ9M6>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+:22
+cls
+pause
+slmgr /ipk <VK7JG-NPHTM-C97JM-9MPGT-3V66T>
+pause
+slmgr /skms kms8.msguides.com
+pause
+slmgr /ato
+pause
+goto :ende
+
+
+:ende 
+cls
+echo Wenn alles richtig funktioniert hat ist jetzt dein Windows aktiviert
+echo Abonniert das Flyteam auf YT https://www.youtube.com/channel/UCQrs3_ulVXGN_ejqdB0thYw
+pause
